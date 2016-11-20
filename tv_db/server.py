601e44915a78712f442b51301408ad61a31c9a0d
dohsimpson <mydohsimpson@gmail.com>
2016-11-20 16:28:55 -0500

    improve

--- tv_db/server.py
+++ tv_db/server.py
@@ -1,6 +1,53 @@
 import json
 from flask import Flask, request, jsonify
 
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            h['Access-Control-Allow-Credentials'] = 'true'
+            h['Access-Control-Allow-Headers'] = \
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 app = Flask(__name__)
 
@@ -89,16 +136,17 @@ def jam():
     return "HELLO WORLD"
 
 
-@app.route('/name', methods=['POST'])
+@app.route('/name', methods=['POST', 'OPTIONS'])
+@crossdomain(origin='*')
 def translate_name_to_id():
     if request.method == 'POST':
-        js = request.json
         form = request.form
-        print js, form
-        import ipdb; ipdb.set_trace()  # TODO: remove this line
-        name = js['name']
+        name = form.items()[0][0]
+        name = name.lower()
         ret = tv2id[name]
-        return jsonify(ret)
+        ret = jsonify({'id': ret})
+        print ret
+        return ret
 
 
 if __name__ == '__main__':
