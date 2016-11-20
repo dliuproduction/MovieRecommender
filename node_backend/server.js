e9dc9f6ee60d40a91f6cee899d66287a526b3299
Dennis Liu <dliuproduction@gmail.com>
2016-11-20 15:13:02 -0500

    query on nodeJS server

--- node_backend/server.js
+++ node_backend/server.js
@@ -7,6 +7,7 @@ var copyTo = require('pg-copy-streams').to;
 var fs = require('fs');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 /**
  * CONFIG for accessing the database
@@ -30,6 +31,22 @@ pool.on('error', function(e, client) {
 });
 
 //check out a client for multiple operations, releasing it when done.
+
+app.use(bodyParser.json())
+
+app.use(function(req,res,next){
+    res.set('Access-Control-Allow-Origin','*');
+    // Request methods you wish to allow
+    res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.set('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.set('Access-Control-Allow-Credentials', true);
+    next()
+})
 
 app.post('/dataQuery', function(req, res) {
     console.log("Got a POST request from client");
