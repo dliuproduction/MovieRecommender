ad4370983ddbf972a1b5cc7e7bdcf65963c4bfe4
zhangrichard <zrc19950205@gmail.com>
2016-11-20 12:14:14 -0500

    add ajax call

---
+++ socket/test.js
@@ -0,0 +1,50 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+// var server = http.createServer(function (request, response) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.end("Hello Worldasdfasdfa\n");
+// });
+
+/* Creating server */
+// var server = http.createServer(function (request, response) {
+//     if (request.url == '/' || request.url == '/index.html') {
+//         var fileStream = fs.createReadStream('./index.html');
+
+//         fileStream.pipe(response);
+//     } else {
+//     	// Website you wish to allow to connect
+// 		// response.setHeader('Access-Control-Allow-Origin', '*');
+//         response.writeHead(200, {"Content-Type": "text/plain"});
+//         response.end("Hello World\n");
+//     }
+// });
+
+app.use(bodyParser.json())
+
+app.use(function(req,res,next){
+	res.set('Access-Control-Allow-Origin','*');
+	// Request methods you wish to allow
+	res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+	// Request headers you wish to allow
+	res.set('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+	// Set to true if you need the website to include cookies in the requests sent
+	// to the API (e.g. in case you use sessions)
+	res.set('Access-Control-Allow-Credentials', true);
+	next()
+})
+app.get('/test.html', function (req, res) {
+  res.sendFile(__dirname+"/test.html")
+})
+app.post('/testing', function (req, res) {
+	console.log(req.body);
+	
+  	res.end('Got a POST request');
+})
+
+/*Start listening*/
+app.listen(3000,function(){
+	console.log("listening to port 3000");
+});
